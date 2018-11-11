5d0769646f3647359aadfedd943a053ba7ab4918
thinkingthigh <thinkingthigh@gmail.com>
2018-11-11 16:21:59 +0800

    添加css loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,14 @@ module.exports = {
     filename: 'bundle.js',
     // 输出文件都放到 dist 目录下
     path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [
+      {
+        // 用正则去匹配要用该 loader 转换的 CSS 文件
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader?minimize'],
+      }
+    ]
   }
 };
